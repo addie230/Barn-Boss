068406b7fd0542ba7113aa7035bd18daabb8146d
addie230 <adrianamaslarova23@gmail.com>
2026-06-16 04:24:26 +0300

    add plant interface

--- code/Plant.h
+++ code/Plant.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "FarmEntity.h"
+
+class Plant : public FarmEntity
+{
+private:
+    int seedId{};
+
+public:
+    Plant(int id, int currentCycle = 0);
+
+    std::string getName() const override;
+    int getKindId() const override 
+    { 
+        return seedId; 
+    }
+    char getCategory() const override 
+    { 
+        return 'P'; 
+    }
+};
