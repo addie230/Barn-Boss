f666bb0bd7015937a287ad243f7e9c0be72858b5
addie230 <adrianamaslarova23@gmail.com>
2026-06-16 14:24:33 +0300

    update barn.h

--- code/Barn.h
+++ code/Barn.h
@@ -1,1 +1,19 @@
 #pragma once
+#include "Product.h"
+#include <map>
+
+class Barn
+{
+private:
+    std::map<ProductType, int> items{};
+
+public:
+    Barn();
+    int get(ProductType product) const;
+    void add(ProductType product, int quantity);
+    bool remove(ProductType product, int quantity);
+    bool has(ProductType product, int quantity) const;
+    void print() const;
+    const std::map<ProductType, int>& getItems() const;
+    void set(ProductType product, int quantity);
+};
