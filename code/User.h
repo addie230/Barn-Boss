f639c96a3cbfdb5a755368d1291133ea64a2d8a7
addie230 <adrianamaslarova23@gmail.com>
2026-06-16 21:37:42 +0300

    add base user abstraction

--- code/User.h
+++ code/User.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <string>
+
+class User
+{
+protected:
+    int id{};
+    std::string username;
+    std::string password;
+
+public:
+    User(int id, std::string username, std::string password);
+    virtual ~User() = default;
+    virtual std::string getType() const = 0;
+    virtual void profileInfo() const;
+    virtual void logout();
+    bool checkPassword(const std::string& password) const;
+    bool changePassword(const std::string& oldPassword, const std::string& newPassword);
+    int getId() const;
+    const std::string& getUsername() const;
+    const std::string& getPassword() const;
+};
